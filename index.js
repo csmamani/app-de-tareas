c530567d442f8b2bd6ed46124a5225ee16bec23e
Carolina Mamani <mamaniccarolina@gmail.com>
2020-11-07 01:51:55 -0300

    addTask function finished

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const task = document.querySelector("input[type=text]");
 const button  = document.querySelector("input[type=button]");
 const tasksList = document.querySelector(".tasks-list")
 
-button.addEventListener("click", addTask);
+let counter = 0;
 
 function addTask () {
-    console.log(task.value)
+    counter++;
+
+    if (!task.value) return;
 
     const listItem = document.createElement("li");
     const text = document.createTextNode(task.value);
-    listItem.appendChild(text);
-    tasksList.appendChild(listItem);
+    const label = document.createElement("LABEL");
+    const checkbox = document.createElement("INPUT");
     listItem.classList.add("task");
+
+    label.setAttribute("for", `task_${counter}`)
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("name", `task_${counter}`);
+
+    label.appendChild(text);
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    tasksList.appendChild(listItem);
+
+    task.value = "";
 }
+
+button.addEventListener("click", addTask);
